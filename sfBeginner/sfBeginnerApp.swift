0329f6f278ccbd503922f0047299a8d2048e6443
stationAstral <cougarjonez@gmail.com>
2023-12-06 15:49:46 -0800

    Initial Commit

---
+++ sfBeginner/sfBeginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  sfBeginnerApp.swift
+//  sfBeginner
+//
+//  Created by stationAstral on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct sfBeginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
