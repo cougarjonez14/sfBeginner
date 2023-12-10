dcfc7daff3aa224d0ca6348cb5e22e813fc2d3ea
stationAstral <cougarjonez@gmail.com>
2023-12-10 05:41:49 -0800

    3

---
+++ sfBeginner/3_Shapes.swift
@@ -0,0 +1,18 @@
+//
+//  3_Shapes.swift
+//  sfBeginner
+//
+//  Created by stationAstral on 12/6/23.
+//
+
+import SwiftUI
+
+struct __Shapes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    __Shapes()
+}
