2f3b989268c4d2355608dc8730f70aceea2efa43
stationAstral <cougarjonez@gmail.com>
2023-12-10 06:05:35 -0800

    4

---
+++ sfBeginner/4_Colors.swift
@@ -0,0 +1,18 @@
+//
+//  4_Colors.swift
+//  sfBeginner
+//
+//  Created by stationAstral on 12/10/23.
+//
+
+import SwiftUI
+
+struct __Colors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    __Colors()
+}
